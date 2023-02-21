405378d3369b68cf1148e0c9c962a4a5c59068fd
Raymond .E Arzu <2016113708@ub.edu.bz>
2023-02-22 10:02:31 +1100

    attempted to install tailwind and also connected main to the database

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,10 +5,13 @@ package main
 import (
 	"context"
 	"database/sql"
-	"flag"
+	"flag"     // allows you to co
 	"log"      // display to terminal
 	"net/http" // create a multiplexer and create routes/endpoints
+	"os"
 	"time"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 // create a new type
@@ -18,11 +21,18 @@ func main() {
 	// create a flag for specifing the port number when starting the server
 
 	addr := flag.String("port", ":4000", "HTTP Network address") //if no port assigned // stored as a pointer
-	flag.Parse()                                                 // this should be called only once
+	dsn := flag.String("dsn", os.Getenv("APPLETREE_DB_DSN"), "PostgresSQL DSN")
+	flag.Parse() // this should be called only once
 	//create instance of application type
-	app := &application{} // creating app type
 
-	// get a rounter
+	db, err := openDB(*dsn)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	app := &application{} // creating app type
+	defer db.Close()
+	log.Println("datase connection pool established.")
 
 	//create a customized server
 	srv := &http.Server{
@@ -32,7 +42,7 @@ func main() {
 
 	log.Printf("Server is active on port %s", *addr)
 
-	err := srv.ListenAndServe() // using the pointer of addr -
+	err = srv.ListenAndServe() // using the pointer of addr -
 	log.Fatal(err)
 
 }
@@ -40,6 +50,7 @@ func main() {
 // Get a database connection pool (2/16/2023)
 
 // this a pointer to the database
+// get a databae connection pool
 func openDB(dsn string) (*sql.DB, error) {
 	db, err := sql.Open("pgx", dsn)
 	if err != nil {
@@ -47,8 +58,8 @@ func openDB(dsn string) (*sql.DB, error) {
 	}
 
 	// use a context to check if the db is reachable (Ping the database) make it safe
-	ctx, cancel := context.WithTimeout(context.Background(),3 * time.Second) // has  a state it is keeping track of
-    defer cancel() // this is always done ... if something was wrong... always execute as the last thing
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second) // has  a state it is keeping track of
+	defer cancel()                                                          // this is always done ... if something was wrong... always execute as the last thing
 
 	err = db.PingContext(ctx)
 	if err != nil {
